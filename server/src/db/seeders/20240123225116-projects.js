ea2a3cdf5889ec3225b52bdad170023abd953758
Paulo Victor Belens <paulovictor55296@gmail.com>
2024-01-23 20:01:53 -0300

    feat: seed for projects created

--- server/src/db/seeders/20240123225116-projects.js
+++ server/src/db/seeders/20240123225116-projects.js
@@ -2,23 +2,29 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('projects', [
+      {
+        title: 'Project 1',
+        tag: 'Tech',
+        url: 'https://project1.com',
+        description: 'Description for Project 1',
+        user_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Project 2',
+        tag: 'Design',
+        url: 'https://project2.com',
+        description: 'Description for Project 2',
+        user_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('projects', null, {});
   }
 };
