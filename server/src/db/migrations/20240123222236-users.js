41c928c1edac9b7d8ac70819179d5ed50ddcaca8
Paulo Victor Belens <paulovictor55296@gmail.com>
2024-01-23 19:25:20 -0300

    feat: migration for users created

---
+++ server/src/db/migrations/20240123222236-users.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      'users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        name: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING,
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        image: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        }
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.dropTable('users');
+  }
+};
