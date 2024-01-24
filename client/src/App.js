e15d224502aebc6f8d23d3466213d43edbfd1326
Paulo Victor Belens <paulovictor55296@gmail.com>
2024-01-24 16:08:59 -0300

    feat: configuring react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 import './App.css';
-import AppBar from './components/AppBar/AppBar';
+
 
 function App() {
   return (
-    <AppBar></AppBar>
+    <BrowserRouter>
+      <Routes>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edite <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+        <Route path="/" exact element={<Login />} />
+
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
