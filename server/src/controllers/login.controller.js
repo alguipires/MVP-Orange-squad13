a4c2a5c798521b0a2a169f6509cb144b400afef0
Paulo Victor Belens <paulovictor55296@gmail.com>
2024-01-24 09:57:00 -0300

    feat: created login controller

---
+++ server/src/controllers/login.controller.js
@@ -0,0 +1,11 @@
+const mapStatusHTTP = require('../utils/mapStatusHTTP');
+
+const loginPost = async (req, res) => {
+  const { email, password } = req.body;
+  const { status, data } = await loginService.loginPostService(email, password)
+  return res.status(mapStatusHTTP(status)).json(data);
+}
+
+module.exports = {
+  loginPost
+}
