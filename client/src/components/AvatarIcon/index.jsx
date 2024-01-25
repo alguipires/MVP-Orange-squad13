b0e0dee33d37d05b8225993a9388f7136cd85979
alguipires <alguipires@gmail.com>
2024-01-25 10:19:21 -0300

    refactor logo, appbar, avatarIcon butons dropdown

--- client/src/components/AvatarIcon/index.jsx
+++ client/src/components/AvatarIcon/index.jsx
@@ -1,8 +1,56 @@
 import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
 import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const settings = ['Perfil', 'Sair'];
 
 // porops nameUser e uriImageUser
-export default function AvatarIcon(props) {
-  return <Avatar alt={props.nameUser} src={props.uriImageUser} />;
+function AvatarIcon(props) {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar alt={props.nameUser} src={props.uriImageUser} />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        {settings.map((setting) => (
+          <MenuItem key={setting} onClick={handleCloseUserMenu}>
+            <Typography textAlign="center">{setting}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
 }
+
+export default AvatarIcon;
