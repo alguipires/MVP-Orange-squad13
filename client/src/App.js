1e0f5b76b3c1bb5753e8d0f5e23dd86b8e4f7768
alguipires <alguipires@gmail.com>
2024-01-25 14:48:58 -0300

    refactor App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
 import AppBar from './components/AppBar/AppBar';
-import InputText from './components/InputText/InputText';
 
 function App() {
-  return (
-    <AppBar />
-    // <InputText placeholder="alo" id="inputId" />
-  );
+  return <AppBar />;
 }
 
 export default App;
