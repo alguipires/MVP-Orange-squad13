6484630b4da100b938196599af99dccb5f1e0f96
Débora Macêdo <deboramacedo886@gmail.com>
2024-01-24 23:16:53 -0300

    criated cards to add projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 import AppBar from './components/AppBar/AppBar';
+import BasicCard from './components/BasicCard/BasicCard';
 
-function App() {
-  return (
-    <AppBar></AppBar>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edite <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+export default function screen(){
+  return(
+    <div>
+      <AppBar/>
+      <BasicCard/>
+    </div>
+  )
 }
-
-export default App;
