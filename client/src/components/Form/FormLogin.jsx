687594fe9e3e319fd6c209721d056969b78d23e1
alguipires <alguipires@gmail.com>
2024-01-25 23:07:06 -0300

    fix conflicts

--- client/src/components/Form/FormLogin.jsx
+++ client/src/components/Form/FormLogin.jsx
@@ -3,11 +3,13 @@ import TextField from '@mui/material/TextField';
 import validator from 'validator';
 import { checkUser } from '../../api/axiosInstance';
 import { useNavigate } from 'react-router-dom';
-import { FormControl, 
-        IconButton, 
-        InputAdornment, 
-        InputLabel, 
-        OutlinedInput } from '@mui/material';
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import './formLogin.css';
@@ -19,7 +21,6 @@ const FormLogin = () => {
   const [isDisabled, setIsDisabled] = useState(true);
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
-
 
   const inputValidation = () => {
     const emailValidation = validator.isEmail(email);
@@ -41,7 +42,7 @@ const FormLogin = () => {
   };
 
   const handleSubmitLogin = async () => {
-    const isValidUser = await checkUser(email, password)
+    const isValidUser = await checkUser(email, password);
 
     if (isValidUser?.token) {
       // salvar o token em algum local
@@ -54,63 +55,55 @@ const FormLogin = () => {
   };
 
   const handleSubmitRegister = async () => {
-      navigate('/register');
-  }   
-
-  const handleSubmit = () => {
-    navigate('/portifolio');
+    navigate('/register');
   };
 
   return (
-    <form  className='form_login'>
+    <form className="form_login">
+      <TextField
+        id="outlined-basic"
+        label="Email address"
+        variant="outlined"
+        className="input_email"
+        onChange={(e) => handleChange(e, setEmail)}
+      />
+      <FormControl style={{ marginTop: 25 }}>
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? 'text' : 'password'}
+          className="input_password"
+          onChange={(e) => handleChange(e, setPassword)}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
 
-    <TextField 
-      id="outlined-basic" 
-      label="Email address" 
-      variant="outlined" 
-      className='input_email'
-      onChange={ (e) => handleChange(e, setEmail) }
-    />
-    <FormControl style={ { marginTop: 25 } }>
+        <Button
+          className="button_login"
+          value="entrar"
+          disabled={isDisabled}
+          onClick={handleSubmitLogin}
+        />
 
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            className='input_password'
-            onChange={ (e) => handleChange(e, setPassword) }
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-
-          <Button 
-            className='button_login'
-            value='entrar'
-            disabled={ isDisabled }  
-            onClick={ handleSubmitLogin }
-            onClick={ handleSubmit }
-          />
-
-          <Button 
-            className='button_register'
-            value='Cadastre-se'
-            onClick={ handleSubmitRegister }
-          />
-
-        </FormControl>
-  </form>
-  )
-}
+        <Button
+          className="button_register"
+          value="Cadastre-se"
+          onClick={handleSubmitRegister}
+        />
+      </FormControl>
+    </form>
+  );
+};
 
 export default FormLogin;
