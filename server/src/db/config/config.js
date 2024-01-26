1f2f2827f0f7dd8e10570a076e101dcaa1a9c3d2
alguipires <alguipires@gmail.com>
2024-01-26 11:18:49 -0300

    config dotenv, alter configurations everioments

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -1,9 +1,9 @@
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: process.env.MYSQL_DB_NAME || 'orange_portifolio_db',
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || '123456',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  database: process.env.MYSQL_DB_NAME,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
