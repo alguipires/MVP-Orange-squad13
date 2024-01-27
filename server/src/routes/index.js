da85f30f280a224e8fe9955551f0dd5a71c494af
alguipires <alguipires@gmail.com>
2024-01-26 22:04:58 -0300

    refactor app routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,20 @@
-const login = require('./login');
+/* const login = require('./login');
 const createUser = require('./create.user')
 
 module.exports = {
   login,
   createUser
-}
+} */
+
+const express = require('express');
+const login = require('./login');
+
+const routes = (app) => {
+  app
+    .route('/')
+    .get((req, res) => res.status(200).send('Running port 3001...'));
+
+  // app.use(express.json(), login);
+};
+
+module.exports = routes;
