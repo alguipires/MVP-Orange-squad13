888bec9fbc02267b526e59865993c6c4d86d6427
alguipires <alguipires@gmail.com>
2024-01-30 02:49:24 -0300

    resolve /portifolio page

---
+++ client/src/pages/Portifolio/Portifolio.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import AppBar from '../AppBar/AppBar';
+import './Portifolio.css';
+// import AvatarIcon from '../AvatarIcon/index';
+import TextField from '../TextFild/TextFild';
+import BasicCard2 from '../BasicCard2/BasicCard2';
+import Profile from '../ProfileHome/ProfileHome';
+import Modal from '../Modal/Modal';
+
+function Portifolio() {
+  return (
+    <section className="styles.portifolio_container">
+      <div>
+        <AppBar />
+      </div>
+      <div className="profile">
+        <Profile />
+      </div>
+      <div className="meusProjetos">Meus Projetos</div>
+      <div className="buscarTags">
+        <TextField />
+      </div>
+      <div>
+        <BasicCard2 />
+      </div>
+      <div>
+        <Modal />
+      </div>
+    </section>
+  );
+}
+export default Portifolio;
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login/Login';
-import Register from "../pages/Register/Register";
+import Register from '../pages/Register/Register';
 import PrivateRoutes from '.';
+import Portifolio from '../pages/Portifolio/Portifolio';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
         <Route path="/" exact element={<Login />} />
 
         <Route path="/register" exact element={<Register />} />
 
-        <Route path='portifolio' element={<PrivateRoutes />} >
-          <Route path='portifolio' element={<h1>Portifolio</h1>} />
+        <Route path="portifolio" element={<PrivateRoutes />}>
+          <Route path="/portifolio" element={<Portifolio />} />
         </Route>
-        
       </Routes>
     </BrowserRouter>
   );
