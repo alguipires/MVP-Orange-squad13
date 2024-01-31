f884513fcf833c32c8e60547b07d296303f544ea
alguipires <alguipires@gmail.com>
2024-01-30 21:13:18 -0300

    resolve user route

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { createUserController } = require('../controllers');
 const userValidation = require('../middlewares/createUserValidation');
+const userGoogleValidation = require('../middlewares/createUserGoogleValidation');
 
 const router = express.Router();
 
-router.post('/user', userValidation, createUserController.createUser);
-//TODO fazer rotas get etc...
+router.post('/', userValidation, createUserController.createUser);
+router.post(
+  '/google',
+  [...userValidation, ...userGoogleValidation],
+  createUserController.createUserWithGoogle
+);
 
 module.exports = router;
