d280ed3fd4faa72ccaaf7bd172af8e3baa90303a
alguipires <alguipires@gmail.com>
2024-02-01 01:24:58 -0300

    install and config lib upload files multer

---
+++ server/config/multer.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/projects/imgs'); // Diretório onde as imgs serão armazenadas
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Nome do arquivo
+    //   cb(null, file.fieldname + '-' + Date.now()) // Nome do arquivo
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
