72bbccb6f78e39076ca147aea6adc49ac1f5fa08
alguipires <alguipires@gmail.com>
2024-02-01 14:57:28 -0300

    refactored delete routes only by usser id or admin role, patch route, and post creation route linked userid and img

--- server/src/routes/project.js
+++ server/src/routes/project.js
@@ -5,24 +5,22 @@ const upload = require('../../config/multer');
 const router = express.Router();
 
 router.get('/', projectController.getAllProjects); //TODO pegar todos os projetos  e paginar
+
 //TODO rota para pegar todos os projetos pelo UserId e paginar (PEGAR PELO USERID E USERUUID)
 router.get('/:id', tokenValidation, projectController.getProjectsbyId); //TODO pegar proj pelo id de projeto
 
-//TODO refatorando com o multer file
-// url nome da fieldname de arquivo no front
 router.post(
   '/',
   tokenValidation,
   upload.single('url'),
   projectController.createProjects
-); //TODO refatorar rota para criar com userId e UserUuid
-
+);
 router.patch(
   '/:id',
   tokenValidation,
   upload.single('url'),
   projectController.updateProjectById
-); //TODO refatorar para editar somento pelo userids vinculado ao projeto
-router.delete('/:id', tokenValidation, projectController.deleteProjectById); //TODO refatorar para deletar somento pelo userids vinculado ao projeto ** OU ADMIN
+);
+router.delete('/:id', tokenValidation, projectController.deleteProjectById);
 
 module.exports = router;
